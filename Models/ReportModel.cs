d5ff559ac6641784f6c2861ad6206da75815b440
MDimitroff <dimitrov.martin.v@gmail.com>
2020-09-28 21:23:22 +0300

    first commit

---
+++ Models/ReportModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ApproachVolume.Models
+{
+    public class ReportModel
+    {
+        public string Interval { get; set; }
+
+        public string Direction { get; set; }
+    }
+}
